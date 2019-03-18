12034ca38453acb9cf72e5a6b7c15e9f653372d2
yuhao <wang.yuhao@campus.lmu.de>
2019-03-18 06:00:53 +0100

    modify regr and cluster datasets boundaries

--- inst/mlrPlayground/server/observe_for_data.R
+++ inst/mlrPlayground/server/observe_for_data.R
@@ -1,8 +1,15 @@
 # set the amount of test data sets
-amount       = 200
+parameter    = reactiveValues(test_ration = 1, scope = 3)
+
+
+observe({
+parameter$test_ration = req(input$test_ration)
+test_ration  = req(parameter$test_ration)
+amount       = round(200 * test_ration)
 
 # by noisy size change data sets
-scope  = 3
+values$scope = req(input$scope)
+scope  = req(values$scope)
 rnom_scope   = rnorm(amount, 0, 1) / scope
 rexp_scope   = rexp(amount, 1) / scope
 runif_scope  = runif(amount, 0, 1) / scope
@@ -12,9 +19,6 @@ runif_scope  = runif(amount, 0, 1) / scope
 # distribution = Null
 # noisy_ration = 0.1
 # size_noisy   = amount * noisy_ration
-
-observe({
-
   req(input$task)
 
   seed = 123 # TODO
@@ -53,7 +57,7 @@ observe({
       #1.add Circle data sets
     } else if (input$task == "1.Circle") {
 
-      angle         = runif(400, 0, 360)
+      angle         = runif(2 * amount, 0, 360)
       radius_class1 = rexp(amount, 1)
       radius_class2 = rnorm(amount, 16, 3)
 
@@ -149,7 +153,7 @@ observe({
       x2_class1     = sqrt(radius_class1) * sin(angle_class1)
       x2_class2     = sqrt(radius_class2) * sin(angle_class2)
       x2            = c(x2_class1, x2_class2)
-      class         = c(rep("Class 1", 1000), rep("Class 2", 1000))
+      class         = c(rep("Class 1", amount * 5), rep("Class 2", amount * 5))
 
       data.frame(
         x1          = x1 * 2, 
@@ -213,40 +217,40 @@ observe({
       #1.add normal linear data sets
     } else if (input$task == "1.Linear ascend") {
 
-      x = rnorm(amount, 0, 5)
-      y = 0.5 * x + (rnom_scope * 5)
+      x = rnorm(amount, 0, 1)
+      y = 0.5 * x + (rnom_scope * 0.5)
 
       data.frame(x, y)
 
       #2.dd Log function data sets
     } else if(input$task == "2.Log linear"){
 
-      x = rnorm(amount * 2, 0, 5)
-      y = log10(2 * x + (rnom_scope * 10))
+      x = abs(rnorm(amount, 0, 1.5))
+      y = 4 * log10(x + abs(rnom_scope))
 
       data.frame(x, y)
 
       #3.add trigonometric function: Sine data sets
     } else if(input$task == "3.Sine"){
 
-      x = c(-amount:amount) * pi/100
-      y = sin(x) + rexp_scope
+      x = c((-1 / 2 * amount) : ((1 / 2 * amount) - 1)) * pi/100
+      y = sin(2 * x) + rexp_scope
 
       data.frame(x,y)
 
       #4.add Ascend Cosine data sets
     } else if(input$task == "4.Ascend Cosine"){
 
-      x = c((-amount) : amount) * pi/100
-      y =  cos(x) + c(1 : (amount * 2 + 1)) / 150 + rexp_scope
+      x = c((1 - amount / 2) : (amount / 2)) * pi/100
+      y = 2 * (cos(2 * x) + c(1 : amount) / 75 + rexp_scope - 2.1)
 
       data.frame(x,y)
 
       #5.add Tangent data sets
     } else if(input$task == "5.Tangent"){
 
-      x = c((-amount + 10) : (amount - 10)) * pi/400
-      y = tan(x) + rnorm((amount - 10) * 2  + 1, -1, 1)  + (rnom_scope * 4)
+      x = c((1-amount / 2) : (amount / 2 )) * pi/230
+      y = tan(x) + (rnom_scope)
 
       data.frame(x, y)
 
@@ -256,9 +260,8 @@ observe({
       sigmoid = function(x) {
         1 / (1 + exp(-x))
       }
-      p = runif(amount * 2, -20, 20)
-      x = p
-      y = sigmoid(p)+ rnom_scope / 2
+      x = runif(amount, -5, 5)
+      y = (8 * sigmoid(3 * x)+ rnom_scope / 4) - 4
 
       data.frame(x,y)
 
@@ -267,28 +270,27 @@ observe({
 
       angle  = runif(amount * 2, 0, 360)
       radius = rnorm(amount * 2, 16, 3)
+      x = sqrt(radius) * cos(2 * pi * angle) + rnom_scope / 3
+      y = sqrt(radius) * sin(2 * pi * angle) + rnom_scope / 2
 
-      data.frame(
-        x = sqrt(radius) * cos(2 * pi * angle) + rnom_scope * 2,
-        y = sqrt(radius) * sin(2 * pi * angle) + rnom_scope * 2
-      )
+      data.frame(x,y)
 
       #8. add Spiral data sets
     } else if(input$task == "8.Spiral"){
 
-      r = c(1: (amount * 2)) / 200 * 5
-      t = 1.75 * c(1: (amount * 2))  / 200 * 2 * pi
-      x = r * sin(t) + rexp_scope * 3
-      y = r * cos(t) + rexp_scope * 3
+      r = c(1 : (amount)) / 200 * 5
+      t = c(1: (amount))  / 200 * 2 * pi
+      x = (r * sin(2 * t) + rexp_scope )
+      y = (r * cos(2 * t) + rexp_scope ) -1 
 
       data.frame(x, y)
 
       #9. add Parabola To Right data sets
     } else if(input$task == "9.Parabola To Right"){
 
-      x  = rexp(amount * 2, 1) + rnom_scope
-      y1 = sqrt(4 * x) + rnom_scope * 2
-      y2 = -sqrt(4 * x) + rnom_scope * 2
+      x  = rexp(amount, 1)%%5
+      y1 = sqrt( x) + rnom_scope /2
+      y2 = -sqrt( x) + rnom_scope /2
       y  = c(y1,y2)
 
       data.frame( x, y)
@@ -296,9 +298,10 @@ observe({
       #10. add Spiral ascend (3D) data sets
     } else if(input$task == "10.Spiral ascend (3D)"){
 
-      z = rexp(amount,1)*4
-      x = sin(z)
-      y = cos(z)
+      z = (rexp(amount, 1)) %% 5
+      x = sin(z * 5) * 3
+      y = cos(z * 5) * 3
+
 
       data.frame(x, y, z)
 
@@ -306,23 +309,23 @@ observe({
       #1. add Clustering Dataset 1
     } else if(input$task == "1.Clustering Dataset 1"){
 
-      x1 = rnorm(amount, 0, 5) + rexp_scope
+      x1 = rnorm(amount, 0, 3)
       x2 = rexp(amount,1) + rexp_scope
       x3 = runif(amount,-2,2) + rexp_scope
-      y1 = rnorm(amount, 0, 5) + rexp_scope
+      y1 = rnorm(amount, 0, 2) 
       y2 = rexp(amount,1) + rexp_scope
       y3 = runif(amount,-2,2) + rexp_scope
 
-      x  = c(x1/3 + 3, x2/3 - 3, x3/3 + 1)
-      y  = c(y1/3 + 3, y2/3 - 1, y3/3 -2)
+      x  = c(x1/3 + 2, x2/3 - 3, x3/3 + 1)
+      y  = c(y1/3 + 2, y2/3 - 1, y3/3 -2)
 
       data.frame(x, y)
 
       #2. add Clustering Dataset 2
     } else if(input$task == "2.Clustering Dataset 2"){
 
-      radius = c(101 : amount) / 200 * 5
-      angle  = 1.75 * c(101 : amount)  / 200 * 2 * pi
+      radius = c(101 : amount) / 60
+      angle  = 1.75 * c(101 : amount)  / 100 * pi
       x      = c(sin(angle), sin(angle + pi)) * radius + rexp_scope
       y      = c(cos(angle), cos(angle + pi)) * radius + rexp_scope
 
@@ -336,28 +339,27 @@ observe({
       p1        = c(0 : (amount - 1)) * pi/200
       p2        = c(100 : (amount + 99)) * pi/200
       p3        = sqrt(radius) * cos(2 * pi * angle)
-      x         = c(p1, p2, p3 - 2) + rexp_scope
+      x         = c(p1, p2, p3 - 2)
 
       y_class1  = sin(p1) + runif(amount, -1, 1) / 4
       y_class2  = sin(p2+pi/2) + runif(amount, -1, 1) / 4
       y_class3  = sqrt(radius) * sin(2*pi*angle)
-      y         = c(y_class1, y_class2, y_class3) + rexp_scope
+      y         = c(y_class1, y_class2, y_class3)
 
       data.frame(x, y)
 
       #4. add Clustering Dataset 4
     } else if(input$task == "4.Clustering Dataset 4"){
 
-      angle         = runif(amount * 2, 0, 360)
+      angle    = runif(amount * 2, 0, 360)
       r_class1 = rnorm(amount, 8, 3)
       r_class2 = rnorm(amount, 32, 3)
       r_class3 = rnorm(amount, 64, 3)
       r_class4 = rnorm(amount, 128, 3)
+      x        = sqrt(abs(c(r_class1, r_class2,r_class3,r_class4))) * cos(2*pi*angle) /3
+      y        = sqrt(c(r_class1, r_class2,r_class3,r_class4)) * sin(2*pi*angle) /3
 
-      data.frame(
-        x = sqrt(c(r_class1, r_class2,r_class3,r_class4)) * cos(2*pi*angle),
-        y = sqrt(c(r_class1, r_class2,r_class3,r_class4)) * sin(2*pi*angle)
-      )
+      data.frame(x, y)
 
       #5. add Clustering Dataset 5
     } else if(input$task == "5.Clustering Dataset 5"){
@@ -365,13 +367,13 @@ observe({
       x1 = rnorm(amount, 0, 5)
       x2 = rnorm(amount, 3, 5) + 4
       x3 = rnorm(amount, 8, 5) - 2
-      x  = c(x1,x2,x3)
+      x  = c(x1,x2,x3) / 5
       y1 = 0.5 * x1 + rnorm(amount, 0, 1)
       y2 = 0.5 * x2 + rnorm(amount, 0, 1) + 6
       y3 = 0.5 * x3 + rnorm(amount, 0, 1) -  6
-      y  = c(y1,y2,y3)
+      y  = c(y1,y2,y3) / 4
 
-      data <- data.frame(x,y)
+      data.frame(x,y)
 
       #6. add Clustering Dataset 6
     } else if(input$task == "6.Clustering Dataset 6"){
@@ -382,13 +384,13 @@ observe({
       angle  = runif(amount, 0, 360)
       radius = rexp(amount, 1)
       x2     = sqrt(radius) * cos(2 * pi * angle) + 2
-      y2     = sqrt(radius) * sin(2 * pi * angle) + 4
+      y2     = sqrt(radius) * sin(2 * pi * angle) + 2
 
-      x3     = sqrt(radius) * cos(2 * pi * angle) + 0.8
-      y3     = sqrt(radius) * sin(2 * pi * angle) - 0.4
+      x3     = sqrt(radius) * cos(2 * pi * angle) + 2
+      y3     = sqrt(radius) * sin(2 * pi * angle) - 2
 
-      x4     = rexp(amount, 1) + runif_scope * 3
-      y4     = c(sqrt(4 * x4),-sqrt(4 * x4)) + runif_scope * 3
+      x4     = (rexp(amount, 1))%%5
+      y4     = c(sqrt(4 * x4),-sqrt(4 * x4)) + rnom_scope 
 
       x      = c(x1, x2, x3, -x4, -x4)
       y      = c(y1, y2, y3, y4)
