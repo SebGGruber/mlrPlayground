4ccf49742b5a0cc4f8d37ac3bd0f7af2f0c1a78e
SebGruber1996 <gruber_sebastian@t-online.de>
2019-03-28 22:36:28 +0100

    i hate travis

--- tests/testthat/test-start-app.R
+++ tests/testthat/test-start-app.R
@@ -2,6 +2,6 @@ context("app-start")
 
 test_that("app start works", {
   # start app
-  expect_silent(shinytest::ShinyDriver$new('../../mlrPlayground', loadTimeout = 30000))
+  expect_silent(shinytest::ShinyDriver$new('../../inst/mlrPlayground', loadTimeout = 30000))
 
 })
